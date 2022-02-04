6b095b43d1aaefe98c119a34740e33ced67c1532
Maruthi158 <77593316+Maruthi158@users.noreply.github.com>
2022-02-04 10:00:01 -0600

    commit

--- A3reducer.py
+++ A3reducer.py
@@ -9,15 +9,15 @@ thisValue = 0
 for line in sys.stdin:
   datalist = line.strip().split('\t')
   if (len(datalist) == 2) : 
-    author, admiration = datalist
+    continent, total_cases = datalist
 
-    if author != thisKey:   # we've moved to another key
+    if continent != thisKey:   # we've moved to another key
       if thisKey:
         # output the previous key-summaryvalue result
         print(thisKey,'\t',thisValue)
 
       # start over for each new key
-      thisKey = author 
+      thisKey = continent 
       thisValue = 0
   
     # apply the aggregation function
